99d8868d0feee8b7a148fbc13847d0a597f898b5
smeea-2018 <smeeaarshad@gmail.com>
2022-06-02 14:33:48 +0100

    read me updated

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const questions = [
     type: "list",
     name: "license",
     message: "Please choose your license",
-    choices: ["MIT", ""],
+    choices: ["MIT", "Apache"],
   },
 
   {
@@ -59,8 +59,61 @@ const init = async () => {
   const answers = await inquirer.prompt(questions);
 
   // display answers
-  console.log(answers);
+  //console.log(answers);
+
+  //generate read me template using the answer
+  //generateReadME(answers);
+  //create readme
 };
 
+const generateReadME = (answers) => {
+  const templateReadMe = `# Project Title ![MIT](https://img.shields.io/badge/MIT-License-green)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${answers.description}
+
+## Installation
+
+Please follow the instructions below:
+
+````npm install````
+
+## Usage
+
+Please follow the instructions below:
+
+\`\`\`npm run start\`\`\`
+
+## License
+
+MIT License
+
+## Contributing
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+
+## Tests
+
+Please follow the instructions below:
+
+\`\`\`npm run test\`\`\`
+
+## Questions
+
+Please contact me on my email: myemail@email.com
+
+Visit my GitHub profile [here](https://github.com/surajverma2587)
+`;
+};
 // 4. start your application
 init();
